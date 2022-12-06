f085604396db1e3c1fbadad8d4f78ce052a12413
cardozogumsc@gmail.com <cardozogumsc@gmail.com>
2022-12-06 09:35:28 -0300

    feat: Adicionando rota do projeto - get

---
+++ core/serializers.py
@@ -0,0 +1,8 @@
+# core/serializers.py
+from rest_framework import serializers
+from .models import Projeto
+class ProjetoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projeto
+        fields = '__all__'
+        
